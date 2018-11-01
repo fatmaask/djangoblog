e69031d182f8084463441269ebf7f54011e15ecc
fatmaask <fatmaasik33@gmail.com>
2018-11-01 22:56:45 +0300

    first commit

---
+++ articles/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.urls import path, re_path
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('', views.article_list, name="list"),
+    path('create/', views.article_create, name="create"),
+    re_path('^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
+]
